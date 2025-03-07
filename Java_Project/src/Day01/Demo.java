73d62bd876e543c4145f4297365c49397d2decd3
aswin <aswin@DESKTOP-SJOO3CN>
2025-03-07 10:31:44 +0530

    extra line added.

--- Java_Project/src/Day01/Demo.java
+++ Java_Project/src/Day01/Demo.java
@@ -10,6 +10,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the First program....");
+		System.out.println("Welcome to the pushed program....");
 		FunctionClass fclass = new FunctionClass();
 		fclass.sound();
 	}
