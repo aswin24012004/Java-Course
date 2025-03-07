855b94bdd686b8fd445d4ebb9e22706622075400
aswin <aswin@DESKTOP-SJOO3CN>
2025-03-07 10:24:45 +0530

    These is  First project is committed.

---
+++ Java_Project/src/Day01/Demo.java
@@ -0,0 +1,17 @@
+package Day01;
+
+class  FunctionClass{
+	void sound() {
+		System.out.println("sound makes....");
+	}
+}
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the First program....");
+		FunctionClass fclass = new FunctionClass();
+		fclass.sound();
+	}
+
+}
